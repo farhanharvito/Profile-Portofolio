d25c0eea61d60e41cf7caa9820ffe1cc9bda0bb4
Harvito <farhanharvito@gmail.com>
2024-05-18 23:26:01 +0700

    Added navbar and styiling

---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+  bg: "#1e2535",
+  bgLight: "#333949",
+  primary: "#2B7CE0",
+  text_primary: "#F2F3F4",
+  text_secondary: "#b1b2b3",
+  card: "#121212",
+  card_light: "#282828",
+  button: "#6d97e8",
+  white: "#FFFFFF",
+  black: "#000000",
+};
+
+export const lightTheme = {
+  bg: "#FFFFFF",
+  bgLight: "#f0f0f0",
+  primary: "#be1adb",
+  text_primary: "#111111",
+  text_secondary: "#48494a",
+  card: "#FFFFFF",
+  button: "#5c5b5b",
+};
